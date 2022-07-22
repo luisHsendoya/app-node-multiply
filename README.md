5b422a3ce136bd110ba9f11aa17981fdebd5e29b
alaska1997 <lsendoya@unal.edu.co>
2022-07-21 20:23:53 -0500

    the file  README.md is ready

---
+++ README.md
@@ -0,0 +1,12 @@
+## Notes:
+To run the application:
+
+`npm install`
+```
+Options:
+      --help     Show help                                            [booleano]
+      --version  Show version number                                  [booleano]
+  -b, --base     is the multiplier             [number]  [required] [default: 10]
+  -n, --num      is the multiplicand           [number] [required]  [default: 1]
+```
+
--- config/argv.js
+++ config/argv.js
@@ -1,24 +1,20 @@
 var argv = require('yargs/yargs')(process.argv.slice(2))
-    .option({
-        'b': {
-            alias: 'base',
-            demandOption: true,
-            default: 10,
-            describe: 'is the num of multiply',
-            type: 'number'
-        }
-    },{
-        'n': {
-            alias: 'num',
-            demandOption: true,
-            default: 1,
-            describe: 'is the base of multiply',
-            type: 'number'
-        }
-    }
-    )
+    .option('b',{
+        alias: 'base',
+        demandOption: true,
+        default: 10,
+        describe: 'is the multiplier',
+        type: 'number'
+    })
+    .option('n',{
+        alias: 'num',
+        demandOption: true,
+        default: 1,
+        describe: 'is the multiplicand',
+        type: 'number'
+    })
     .argv
-;
+
 
 
 module.exports=argv;
